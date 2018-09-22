2384a9f9bda15887b1e591c25bf40f6abe4792e2
Emily Sayle <emsayle@gmail.com>
2018-09-22 12:21:14 +0100

    added test to set a name property and implemented code

--- __tests__/cat.tests.js
+++ __tests__/cat.tests.js
@@ -4,4 +4,16 @@ describe('Cat', () => {
   it('creates an instance of Cat', () => {
     expect(new Cat()).toBeInstanceOf(Object);
   });
+  it('sets the name property', () => {
+    const cat = new Cat('Mr Whiskerson');
+    expect(cat.name).toEqual('Mr Whiskerson');
+  });
+});
+
+describe('growUp function', () => {
+  it('growUp() increases the cat age by 1', () => {
+    const cat = new Cat('Mr Whiskerson');
+    cat.growUp();
+    expect(cat.age).toEqual(1);
+  });
 });
--- src/cat.js
+++ src/cat.js
@@ -1,5 +1,10 @@
-function Cat () {
+function Cat(name) {
+  this.name = name;
+  this.age = 0;
+};
 
+Cat.prototype.growUp = function() {
+  this.age += 1;
 }
 
 module.exports = Cat;
